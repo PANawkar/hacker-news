5448ce0183013293001fc8faa8c21c0df8fe7059
PANawkar <pratiknawkar94@gmail.com>
2024-01-31 22:33:07 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ function App() {
 
 	return (
 		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
+			{user && <Route path="/" exact element={<Main/>} />}
+			<Route path="/signup" exact element={<Signup/>} />
+			<Route path="/login" exact element={<Login/>} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 		</Routes>
 	);
